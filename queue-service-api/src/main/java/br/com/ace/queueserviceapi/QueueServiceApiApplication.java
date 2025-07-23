1ae96ce3593bd6b59b706eb8b741f755c777aaa0
Ace0777 <luishhasantos@gmail.com>
2025-07-23 07:43:49 -0300

    chore: add config server connection

---
+++ queue-service-api/src/main/java/br/com/ace/queueserviceapi/QueueServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ace.queueserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueServiceApiApplication.class, args);
+	}
+
+}
