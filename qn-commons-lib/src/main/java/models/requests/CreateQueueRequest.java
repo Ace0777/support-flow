7060a4b81e5c6270b98c73d2ce97230c928efb5b
Ace0777 <luishhasantos@gmail.com>
2025-07-26 16:41:43 -0300

    feat: Create queue request structure

---
+++ qn-commons-lib/src/main/java/models/requests/CreateQueueRequest.java
@@ -0,0 +1,20 @@
+package models.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateQueueRequest(
+
+        @NotBlank(message = "Requester ID cannot be empty")
+        String requesterIdm,
+        @NotBlank(message = "Customer ID cannot be empty")
+        String customerId,
+        @Size(min = 3, max = 50, message = "Title must be between 3 and 50 characters")
+        String title,
+        @Size(min = 3, max = 100, message = "Description must be between 3 and 100 characters")
+        String description,
+        @NotBlank(message = "Status cannot be empty")
+        String status
+
+) {
+}
---
+++ queue-service-api/src/main/java/br/com/ace/queueserviceapi/services/QueueService.java
@@ -0,0 +1,6 @@
+package br.com.ace.queueserviceapi.services;
+
+public interface QueueService {
+
+
+}
