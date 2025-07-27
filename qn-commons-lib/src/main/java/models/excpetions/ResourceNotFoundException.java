a1a6007aab8b99f002245ea43f3d48c81dbfd8b9
Luís Henrique <luishhasantos@gmail.com>
2025-07-26 22:19:38 -0300

    Delete qn-commons-lib directory

--- qn-commons-lib/src/main/java/models/excpetions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package models.excpetions;
-
-public class ResourceNotFoundException  extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
