be7e19a2583a54eb1d877882704448acd3a6f4cf
Ace0777 <luishhasantos@gmail.com>
2025-08-05 06:07:37 -0300

    chore: start supportflow bff project

---
+++ supportflow-bff/src/main/java/br/com/ace/supportflowbff/SupportflowBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.ace.supportflowbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportflowBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportflowBffApplication.class, args);
+	}
+
+}
---
+++ supportflow-bff/src/test/java/br/com/ace/supportflowbff/SupportflowBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ace.supportflowbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportflowBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
