c350ed9d6bd04cd6b75f883a7b31060a939c90cd
Ace0777 <luishhasantos@gmail.com>
2025-08-12 20:21:22 -0300

    feat: add decoder for errors

---
+++ sf-commons-lib/src/main/java/models/excpetions/InternalServerErrorException.java
@@ -0,0 +1,7 @@
+package models.excpetions;
+
+public class InternalServerErrorException extends RuntimeException{
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+}
